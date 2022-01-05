b254c3904c5fb108cd34db9973fe8d1562061634
Hitalo <hitalo331@outlook.com>
2022-01-05 11:37:19 -0300

    treewide: some improvements for my use

--- generate.py
+++ generate.py
@@ -1,21 +1,18 @@
-import sys
-import json
 import os
+import sys
+
+import rapidjson as json
 from github import Github
 
 # Configuration
-REPO_NAME = "Magisk-Modules-Alt-Repo"
-REPO_TITLE = "Magisk Modules Alt Repo"
+REPO_NAME = "Magisk-Modules-Repo"
+REPO_TITLE = "Magisk Modules Repo"
 
 # Skeleton for the repository
-meta = {
-    "name": REPO_TITLE,
-    "last_update": "",
-    "modules": []
-}
+meta = {"name": REPO_TITLE, "last_update": "", "modules": []}
 
 # Initialize the GitHub objects
-g = Github(os.environ['GIT_TOKEN'])
+g = Github(os.environ["GH_TOKEN"])
 user = g.get_user(REPO_NAME)
 repos = user.get_repos()
 
@@ -27,21 +24,23 @@ for repo in repos:
     # It is possible that module.prop does not exist (meta repo)
     try:
         # Parse module.prop into a python object
-        moduleprop_raw = repo.get_contents("module.prop").decoded_content.decode("UTF-8")
+        moduleprop_raw = repo.get_contents("module.prop").decoded_content.decode(
+            "UTF-8"
+        )
         moduleprop = {}
         for line in moduleprop_raw.splitlines():
             if "=" not in line:
                 continue
             lhs, rhs = line.split("=", 1)
             moduleprop[lhs] = rhs
-        
+
         # Create meta module information
         module = {
             "id": moduleprop["id"],
             "last_update": int(repo.updated_at.timestamp() * 1000),
             "prop_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/module.prop",
             "zip_url": f"https://github.com/{repo.full_name}/archive/{repo.default_branch}.zip",
-            "notes_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/README.md"
+            "notes_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/README.md",
         }
 
         # Append to skeleton
@@ -49,5 +48,7 @@ for repo in repos:
     except:
         continue
 
-# Return our final skeleton
-print(json.dumps(meta, indent=4, sort_keys=True))
+# Save our final skeleton to modules.json
+jdump = json.dumps(meta, indent=4, sort_keys=True)
+with open("modules.json", "w", encoding="utf-8") as f:
+    f.write(jdump)
